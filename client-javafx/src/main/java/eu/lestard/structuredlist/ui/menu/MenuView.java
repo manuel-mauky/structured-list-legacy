54c2e684f3c14bfa03efeffed8d325acc4e81db5
Manuel Mauky <manuel.mauky@saxsys.de>
2015-07-14 18:52:18 +0200

    ItemDialog in Menu

--- client-javafx/src/main/java/eu/lestard/structuredlist/ui/menu/MenuView.java
+++ client-javafx/src/main/java/eu/lestard/structuredlist/ui/menu/MenuView.java
@@ -3,6 +3,7 @@ package eu.lestard.structuredlist.ui.menu;
 import eu.lestard.fluxfx.View;
 import eu.lestard.structuredlist.actions.ExitApplicationAction;
 import eu.lestard.structuredlist.actions.NewItemAction;
+import eu.lestard.structuredlist.ui.itemoverview.ItemInputDialog;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextInputDialog;
 
@@ -10,9 +11,8 @@ public class MenuView implements View {
 
     @FXML
     public void newItem(){
-        TextInputDialog dialog = new TextInputDialog();
-        dialog.setTitle("Add new Item");
-        dialog.setHeaderText(null);
+		
+        ItemInputDialog dialog = new ItemInputDialog("Add new Item");
         dialog.showAndWait().ifPresent(text -> publishAction(new NewItemAction(text)));
     }
 
